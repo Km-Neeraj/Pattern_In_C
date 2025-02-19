e61b276ff9bb494a84a21580f062077e9f627a9b
Neeraj Yadav <neerajydv1113@gmail.com>
2025-02-19 09:37:11 +0530

    Add files via upload

---
+++ HollowFullPyramidPattern.c
@@ -0,0 +1,35 @@
+ /*     *          
+      *   *         
+    *       *       
+  *           *     
+* * * * * * * * *   (HollowFullStarPyramid) */
+ 
+ #include<stdio.h>
+ int main()
+ {
+     int row;
+     printf("Enter the number of row\n");
+     scanf("%d",&row);
+     for(int i=1;i<=row;i++)
+     {
+         for(int j=1;j<=2*row-2*i;j++)
+         {
+              printf(" ");
+         }
+         for(int k=1;k<=2*i-1;k++)
+         {
+             if(i==1||i==row||k==1||k==2*i-1)
+             {
+                printf("* ");
+             }
+             else
+             {
+               printf("  ");
+             }
+         }
+         printf("\n");
+     }
+     return 0;
+ }
+
+ 
