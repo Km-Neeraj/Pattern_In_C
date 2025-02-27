b126671174285927812a90b42ae3779b7cba24be
Neeraj Yadav <neerajydv1113@gmail.com>
2025-02-27 11:15:33 +0530

    Add files via upload

---
+++ HutPattern.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+   int n;
+   printf("Enter the number of lines\n");
+   scanf("%d",&n);
+   for(int i=1;i<=n;i++)
+   {
+        for(int k=n;k>=i;k--)
+        {
+           printf(" ");
+        }
+      for(int j=1;j<=i;j++)
+      {
+        printf("* ");
+      }
+      printf("\n");
+}
+      for(int i=1;i<=n+1;i++)
+{
+    for(int j=1;j<=n+1;j++)
+    {
+       printf("* ");
+    }
+    printf("\n");
+
+}
+   return 0;
+}
+
+/*                  * 
+                   * * 
+                  * * * 
+                 * * * * 
+                * * * * * 
+               * * * * * * 
+               * * * * * * 
+               * * * * * * 
+               * * * * * * 
+               * * * * * *
+               * * * * * *      */
